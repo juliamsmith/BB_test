5625a927cbc4fe3c13b2c0faccead867e3e103e3
juliamsmith <jmsmith20@uchicago.edu>
2018-11-03 22:51:47 -0500

    small changes to run new conditions

--- to_generate/terminal_call.py
+++ to_generate/terminal_call.py
@@ -3,9 +3,9 @@ import numpy
 
 if __name__ == "__main__":
     #this is what gets editted most of the time -- also potentially writer
-    dim_vec=[2250]#[900, 2250, 4500, 9000] #the equivalent of grid [100, 250, 500, 1000]
+    dim_vec=[450, 900, 2250, 4500, 9000] #the equivalent of grid [100, 250, 500, 1000] #50m apart, 100, 250, 500, 1000
     m_prop_vec=numpy.linspace(.05, .95, 10)#numpy.linspace(.05, .95, 19) #.05, .1, .15, ... , .95; OR: intervals of .1
-    RB_time_vec=[4]#[.5, 2, 4, 6] #hrs
+    RB_time_vec=[.5, 3, 6, 12, 16, 24] #hrs
     num_sims=1000
-    max_m_vec=[.004, .009, .018, .037, .057]
+    max_m_vec=[.018]#[.004, .009, .018, .037, .057]
     vary_params(dim_vec, m_prop_vec, RB_time_vec, num_sims, max_m_vec)
