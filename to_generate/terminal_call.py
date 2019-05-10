aa81e80cd0d044845b86c93520ddd7b54ea98b6a
Rachel Potter <rachelbpotter@midway2-login2.rcc.local>
2019-05-10 18:57:47 -0500

    multi-male spatial

--- to_generate/terminal_call.py
+++ to_generate/terminal_call.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     m_prop_vec = np.linspace(.05, .95, 10)#numpy.linspace(.05, .95, 19) #.05, .1, .15, ... , .95; OR: intervals of .1
     RB_time_vec=[6] #hrs
     n_males_vec=[4,6,8,12,24]
-    num_sims=2
+    num_sims=1000
     max_m_vec=[0.054] #one succesful attempt a day
     vary_params(dist_vec, m_prop_vec, RB_time_vec, num_sims, max_m_vec, n_males_vec)
